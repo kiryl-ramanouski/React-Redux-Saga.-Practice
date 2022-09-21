b59ba60bac8dad709199f8e3f68248f39ac882f9
Kiryl Ramanouski <whitekvazar@gmail.com>
2022-09-21 16:17:04 +0300

    feat: update PostForm component

--- react-app/src/Components/PostForm.js
+++ react-app/src/Components/PostForm.js
@@ -4,19 +4,23 @@ export default class PostForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.submitHandler = this.submitHandler.bind(this);
   }
 
-  submitHandler(e) {
-    e.preventDefault();
-  }
+  submitHandler = (event) => {
+    event.preventDefault();
+  };
 
   render() {
     return (
-      <>
-        <h1>Post Form</h1>
-        <form onSubmit={this.submitHandler}></form>
-      </>
+      <form onSubmit={this.submitHandler}>
+        <div className="form-group">
+          <label htmlFor="title">Post Title</label>
+          <input type="text" id="title" className="form-control" />
+        </div>
+        <button type="submit" className="btn btn-success">
+          Create
+        </button>
+      </form>
     );
   }
 }
