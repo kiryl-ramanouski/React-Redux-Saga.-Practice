605e01c8464f61b6530aab976a259ddb3a7d4a41
Kiryl Ramanouski <whitekvazar@gmail.com>
2022-09-22 22:27:30 +0300

    feat: create fetchPosts async action-creator

--- react-app/src/Redux/actions.js
+++ react-app/src/Redux/actions.js
@@ -1,8 +1,28 @@
 import { CREATE_POST } from './types';
+import { FETCH_POSTS } from './types';
 
 export const createPost = (post) => {
   return {
     type: CREATE_POST,
     payload: post,
   };
+};
+
+export const fetchPosts = () => {
+  return async (dispatch) => {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/posts?_limit=5'
+    );
+    const json = await response.json();
+    return dispatch({ type: FETCH_POSTS, payload: json });
+  };
+
+  // Is it also correct way to get posts or not?
+  // const response = async () =>
+  //   await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5');
+  // const posts = response.json();
+  // return {
+  //   type: FETCH_POSTS,
+  //   payload: posts,
+  // };
 };
--- react-app/src/Redux/types.js
+++ react-app/src/Redux/types.js
@@ -1,1 +1,5 @@
-export const CREATE_POST = 'CREATE_POST';
+export const CREATE_POST = 'POSTS/CREATE_POST';
+export const FETCH_POSTS = 'POSTS/FETCH_POSTS';
+
+export const SHOW_LOADER = 'APP/SHOW_LOADER';
+export const HIDE_LOADER = 'APP/HIDE_LOADER';
