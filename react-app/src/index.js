ccc0a7fc78916b3aa73f4b69b4f3eaf93010b9b8
Kiryl Ramanouski <whitekvazar@gmail.com>
2022-09-22 22:30:45 +0300

    feat: add thunk to middleware

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { compose, createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { rootReducer } from './Redux/rootReducer';
+import thunk from 'redux-thunk';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { compose, createStore } from 'redux';
-import { rootReducer } from './Redux/rootReducer';
-import { Provider } from 'react-redux';
 
 const store = createStore(
   rootReducer,
   compose(
+    applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
