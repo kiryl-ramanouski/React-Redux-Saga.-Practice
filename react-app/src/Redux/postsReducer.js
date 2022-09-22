dfd14cb41f7129562f124586a867c89fe1720a4e
Kiryl Ramanouski <whitekvazar@gmail.com>
2022-09-22 16:14:59 +0300

    feat: add reducer for createPost

--- react-app/src/Redux/postsReducer.js
+++ react-app/src/Redux/postsReducer.js
@@ -1,6 +1,16 @@
+import { CREATE_POST } from './types';
+
 const initialState = {
-  posts: [1, 2],
+  posts: [],
   fetchedPosts: [],
 };
 
-export const postsReducer = (state = initialState, action) => state;
+export const postsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_POST:
+      return { ...state, posts: [...state.posts, action.payload] }; // variant one
+    // return { ...state, posts: state.posts.concat(action.payload) }; // variant two
+    default:
+      return state;
+  }
+};
