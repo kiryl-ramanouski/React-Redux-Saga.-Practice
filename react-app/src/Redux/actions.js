c946e64f0d24575a7e7329bde1a7e5179caf9e20
Kiryl Ramanouski <whitekvazar@gmail.com>
2022-09-23 15:42:29 +0300

    feat: create show and hide loader action-creators

--- react-app/src/Redux/actions.js
+++ react-app/src/Redux/actions.js
@@ -1,5 +1,7 @@
 import { CREATE_POST } from './types';
 import { FETCH_POSTS } from './types';
+import { SHOW_LOADER } from './types';
+import { HIDE_LOADER } from './types';
 
 export const createPost = (post) => {
   return {
@@ -8,15 +10,26 @@ export const createPost = (post) => {
   };
 };
 
+export const showLoader = () => {
+  return {
+    type: SHOW_LOADER,
+  };
+};
+
+export const hideLoader = () => {
+  return {
+    type: HIDE_LOADER,
+  };
+};
+
 export const fetchPosts = () => {
   return async (dispatch) => {
+    dispatch(showLoader());
     const response = await fetch(
       'https://jsonplaceholder.typicode.com/posts?_limit=5'
     );
     const posts = await response.json();
-    return dispatch({
-      type: FETCH_POSTS,
-      payload: posts,
-    });
+    dispatch({ type: FETCH_POSTS, payload: posts });
+    dispatch(hideLoader());
   };
 };
