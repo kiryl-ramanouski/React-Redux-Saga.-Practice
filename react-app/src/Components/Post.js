72bc67e63f240486237f7b6500b18066eed5b830
Kiryl Ramanouski <whitekvazar@gmail.com>
2022-09-23 15:44:00 +0300

    refactor: Post

--- react-app/src/Components/Post.js
+++ react-app/src/Components/Post.js
@@ -5,7 +5,7 @@ export default ({ title }) => {
   return (
     <div className="card">
       <div className="card-body">
-        <h5 className="card-title">Title for {title}</h5>
+        <h5 className="card-title">{title}</h5>
       </div>
     </div>
   );
