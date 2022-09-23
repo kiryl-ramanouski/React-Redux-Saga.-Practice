78fd695a4765f0434c7496d43d926f870cb2967c
Kiryl Ramanouski <whitekvazar@gmail.com>
2022-09-23 15:44:29 +0300

    feat: create Alert component

---
+++ react-app/src/Components/Alert.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Alert = () => {
+  return (
+    <div class="alert alert-danger" role="alert">
+      Something went wrong. Check Your Internet Connection and reload the page.
+    </div>
+  );
+};
